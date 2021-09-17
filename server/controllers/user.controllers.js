843f366f179a403ae218f17efa98d866d7721ead
Haiquan Zheng <hqzktb@gmail.com>
2021-09-17 16:11:15 -0500

    committing user models

--- server/controllers/user.controllers.js
+++ server/controllers/user.controllers.js
@@ -1,8 +1,105 @@
-const Users = require("../models/user.model");
+const User = require("../models/user.model");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 module.exports = {
-  register: () => {},
-  login: () => {},
-  update: () => {},
-  findOne: () => {},
+  register: async (req, res) => {
+    try {
+      const queriedUser = await User.findOne({ username: req.body.username });
+      if (queriedUser) {
+        res.status(400).json({ message: "username already in use" });
+        return;
+      }
+    } catch (err) {
+      res.status(400).json(err);
+    }
+
+    const newUser = new User(req.body);
+    try {
+      const newUserObj = await newUser.save();
+      const usertoken = jwt.sign(
+        { id: newUserObj._id },
+        process.env.SECRET_KEY
+      );
+
+      res
+        .cookie("usertoken", usertoken, process.env.SECRET_KEY, {
+          httpOnly: true,
+        })
+        .json(newUserObj);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  },
+
+  login: async (req, res) => {
+    let queriedUser;
+    try {
+      queriedUser = await User.findOne({ username: req.body.username });
+      if (!queriedUser) {
+        res.status(400).json({ message: "User not found!" });
+        return;
+      }
+    } catch (err) {
+      res.status(400).json(err);
+      return;
+    }
+
+    const passwordCheck = bcrypt.compareSync(
+      req.body.password,
+      queriedUser.password
+    );
+    if (!passwordCheck) {
+      res.status(400).json({ message: "Password does not match!" });
+      return;
+    }
+
+    const usertoken = jwt.sign({ id: queriedUser._id }, process.env.SECRET_KEY);
+    res
+      .cookie("usertoken", usertoken, process.env.SECRET_KEY, {
+        httpOnly: true,
+      })
+      .json(queriedUser);
+  },
+
+  logout: (req, res) => {
+    res.clearCookie("usertoken");
+    res.json({ message: "logout successful" });
+  },
+
+  update: async (req, res) => {
+    let queriedUser;
+    try {
+      queriedUser = await User.findOne({ username: req.body.username });
+      if (!queriedUser) {
+        res.status(400).json({ message: "user not found" });
+        return;
+      }
+    } catch (err) {
+      res.status(400).json(err);
+      return;
+    }
+
+    try {
+      const updatedUser = await User.findOneAndUpdate(
+        { username: req.body.username },
+        req.body,
+        { new: true }
+      );
+      res.json(updatedUser);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  },
+
+  findOne: async (req, res) => {
+    const decodedJwt = jwt.decode(req.cookies.usertoken, { complete: true });
+    try {
+      const queriedUser = User.findOne({ _id: decodedJwt.payload.id });
+      res.json(queriedUser);
+      return;
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  },
 };
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,8 +1,10 @@
 const UserController = require("../controllers/user.controllers");
+const { authenticate } = require("../middleware/jwt.middleware");
 
 module.exports = (app) => {
-  app.get("/api/users/getOne/:id", UserController.findOne);
-  app.post("/api/users/register", UserController.register);
-  app.post("/api/users/login", UserController.login);
-  app.put("/api/users/update", UserController.update);
+  app.get("/api/users/getOne/:id", authenticate, UserController.findOne);
+  app.post("/api/users/register", authenticate, UserController.register);
+  app.post("/api/users/login", authenticate, UserController.login);
+  app.put("/api/users/logout", authenticate, UserController.logout);
+  app.put("/api/users/update", authenticate, UserController.update);
 };
