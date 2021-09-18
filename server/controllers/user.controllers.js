b53ea78ad64732eb5fd06743afb6ec5b46804899
gshelly <gshelly1402@gmail.com>
2021-09-17 17:01:57 -0700

    updated env framework

--- server/controllers/user.controllers.js
+++ server/controllers/user.controllers.js
@@ -1,5 +1,5 @@
 const User = require("../models/user.model");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 module.exports = {
@@ -21,7 +21,6 @@ module.exports = {
         { id: newUserObj._id },
         process.env.SECRET_KEY
       );
-
       res
         .cookie("usertoken", usertoken, process.env.SECRET_KEY, {
           httpOnly: true,
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -2,9 +2,9 @@ const UserController = require("../controllers/user.controllers");
 const { authenticate } = require("../middleware/jwt.middleware");
 
 module.exports = (app) => {
-  app.get("/api/users/getOne/:id", authenticate, UserController.findOne);
-  app.post("/api/users/register", authenticate, UserController.register);
-  app.post("/api/users/login", authenticate, UserController.login);
+  app.get("/api/users/getOne/:id", UserController.findOne);
+  app.post("/api/users/register", UserController.register);
+  app.post("/api/users/login", UserController.login);
   app.put("/api/users/logout", authenticate, UserController.logout);
   app.put("/api/users/update", authenticate, UserController.update);
 };
